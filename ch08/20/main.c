cf4473cc2ac662b6fc8dfe83c0f1f08449b091e7
atsu <atsushi.tohyama.160.333@gmail.com>
2021-06-28 21:47:27 +0900

    Update 8_20

--- ch08/20/main.c
+++ ch08/20/main.c
@@ -1,6 +1,13 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]) {
     char *ls_path = "/bin/ls";
-    execve(ls_path, argv, envp);
+    if (execve(ls_path, argv, envp) == -1) {
+        fprintf(stderr, "Fail to execve: %s\n", strerror(errno));
+        exit(1);
+    }
 }
