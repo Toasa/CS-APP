38ed522f582d48759c56510245b125fbc70e49e9
atsu <atsushi.tohyama.160.333@gmail.com>
2021-07-25 20:43:29 +0900

    Add 2.56

---
+++ ch02/56/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    for (size_t i = 0; i < len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main() {
+    int x = 0x12345678;
+    float f = (float)x;
+    show_int(x);
+    show_float(f);
+    show_pointer(&x);
+    show_pointer(&f);
+}
