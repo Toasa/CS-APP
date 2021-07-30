9a872d4e35aecd977b448a7e6f961211660e96ac
atsu <atsushi.tohyama.160.333@gmail.com>
2021-07-30 09:32:12 +0900

    Add bit utility

---
+++ ch02/util.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+
+void printb(unsigned int v) {
+  unsigned int mask = (int)1 << (sizeof(v) * CHAR_BIT - 1);
+  int count = 0;
+  do {
+      putchar(mask & v ? '1' : '0');
+      count++;
+      if (count == 8 || count == 16 || count == 24)
+          printf("_");
+  } while (mask >>= 1);
+  printf("\n");
+}
