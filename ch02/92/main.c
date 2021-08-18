22db86519a588115afed4ad949873505d6035577
atsu <atsushi.tohyama.160.333@gmail.com>
2021-08-18 21:32:07 +0900

    Improve test of 2.92

--- ch02/92/main.c
+++ ch02/92/main.c
@@ -1,4 +1,15 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
 typedef unsigned float_bits;
+
+float float_negate_f(float f) {
+    if (isnan(f))
+        return f;
+    else
+        return -f;
+}
 
 // Compute -f. If f is NaN, then return f.
 float_bits float_negate(float_bits f) {
@@ -13,4 +24,27 @@ float_bits float_negate(float_bits f) {
 
     unsigned inverted = !sign;
     return (inverted << 31) | (exp << 23) | frac;
+}
+
+int main() {
+    uint64_t bad = 0;
+    uint64_t good = 0;
+
+    unsigned x = 0;
+
+    do {
+        float *fp = (float *)&x;
+        float f = float_negate_f(*fp);
+        float_bits fb = float_negate(x);
+
+        unsigned *ff = (unsigned *)&f;
+        if (*ff != fb) {
+            bad++;
+            printf("%f (%.8x): %.8x != %.8x\n", *fp, x, fb, *ff);
+        } else {
+            good++;
+        }
+    } while (++x);
+
+    printf("good=%lu, bad=%lu\n", good, bad);
 }
