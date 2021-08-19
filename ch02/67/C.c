b3d090c97ccebb0de40f1bea77c9eece88934b3b
atsu <atsushi.tohyama.160.333@gmail.com>
2021-08-19 11:28:50 +0900

    Add 2.67

---
+++ ch02/67/C.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+
+int int_size_is_32() {
+    int set_msb = 1 << 15 << 15 << 1;
+    int beyond_msb = set_msb << 1;
+
+    return set_msb && !beyond_msb;
+}
+
+int main() {
+    assert(int_size_is_32() == 1);
+}
